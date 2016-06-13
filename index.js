d18f1e32b308a74ab04cab7d82647930a3e6521e
Melanie Gin <mgin@etsy.com>
2016-06-12 22:50:30 -0700

    Deploy on Node server for now

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function (req, res) {
+  res.send('client/index.html');
+});
